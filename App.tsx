13b7c43eeadc01459724f70388e62943163eb0e2
HarithaGane99 <haritha.99ganegoda@gmail.com>
2024-02-21 10:14:20 +0530

    Before final assesment

--- App.tsx
+++ App.tsx
@@ -1,46 +1,30 @@
-/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable prettier/prettier */
-import React, { Component } from 'react';
-import {  Image, StyleSheet, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import LittleLemonHeader from './src/components/LittleLemonHeader';
 import LittleLemonFooter from './src/components/LittleLemonFooter';
-import WelcomeScreen from './src/components/WelcomeScreen';
-import MenuItems from './src/components/MenuItems';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/components/LoginScreen';
+import WelcomeScreen from './src/components/WelcomeScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
-
-
-export class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <View style={styles.container}>
-          <LittleLemonHeader/>
-            <Stack.Navigator 
-              initialRouteName='LogIn' 
-              screenOptions={{
-                headerStyle:{backgroundColor:'#03fc2c'},
-                headerTintColor:'#fff',
-                headerTitleStyle:{
-                  fontWeight:'bold',
-                }
-            }}>
-              <Stack.Screen name='LogIn' component={LoginScreen}/>
-              <Stack.Screen name='Welcome' component={WelcomeScreen}/>
-              <Stack.Screen name='Menu' component={MenuItems}/>
-            </Stack.Navigator>
-          <LittleLemonFooter/>
-        </View>
-      </NavigationContainer>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+        <LittleLemonFooter />
+      </View>
+    </NavigationContainer>
+  );
 }
 
-export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,4 +32,3 @@ const styles = StyleSheet.create({
   },
   footerContainer: { backgroundColor: '#333333' },
 });
-
